7c6b4693feefb06f319e1e7eb04777a5bf1e42d8
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-07-21 16:26:38 -0300

    refactor: create Hero section component

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,10 @@
+import { ProfileCover } from "./ProfileCover";
+
+export default function Hero() {
+    return (
+        <div className="mx-auto md:max-w-3xl">
+            <ProfileCover />
+        </div>
+
+    )
+}
