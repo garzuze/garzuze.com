e6b2211a8b0fce91166c48a9a39c313e3d835075
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-07-21 16:26:24 -0300

    feat: create Garzuze Mark component

---
+++ src/app/components/GarzuzeMark.tsx
@@ -0,0 +1,54 @@
+const GarzuzeMark = (props: React.ComponentProps<"svg">) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 810 810"
+            {...props}
+        >
+            <defs />
+            <g id="54d4560962">
+                <g>
+                    <g transform="translate(0.0000035, 571.008736)">
+                        <path
+                            d="M 60 -270 L 90 -270 L 90 -210 L 60 -210 Z M 60 -150 L 90 -150 L 90 60 L 60 60 Z M 0 60 L 60 60 L 60 90 L 0 90 Z M 0 60"
+                        />
+                    </g>
+                </g>
+
+                <g>
+                    <g transform="translate(119.992578, 571.008736)">
+                        <path
+                            d="M 60 -120 L 60 -150 L 150 -150 L 150 -120 Z M 60 -30 L 30 -30 L 30 -120 L 60 -120 Z M 150 -120 L 180 -120 L 180 60 L 150 60 L 150 0 L 60 0 L 60 -30 L 150 -30 Z M 150 60 L 150 90 L 60 90 L 60 60 Z M 150 60"
+                        />
+                    </g>
+                </g>
+
+                <g>
+                    <g transform="translate(329.979586, 571.008736)">
+                        <path
+                            d="M 60 -270 L 90 -270 L 90 -210 L 60 -210 Z M 60 -150 L 90 -150 L 90 60 L 60 60 Z M 0 60 L 60 60 L 60 90 L 0 90 Z M 0 60"
+                        />
+                    </g>
+                </g>
+
+                <g>
+                    <g transform="translate(449.972161, 571.008736)">
+                        <path
+                            d="M 165 -30 L 165 0 L 90 0 L 90 60 L 60 60 L 60 -150 L 90 -150 L 90 -30 Z M 0 60 L 60 60 L 60 90 L 0 90 Z M 0 60"
+                        />
+                    </g>
+                </g>
+
+                <g>
+                    <g transform="translate(599.96288, 571.008736)">
+                        <path
+                            d="M 120 -210 L 90 -210 L 90 -270 L 120 -270 Z M 60 -120 L 60 -150 L 150 -150 L 150 -120 Z M 180 -30 L 180 0 L -15 0 L -15 -30 L 30 -30 L 30 -120 L 60 -120 L 60 -30 Z M 180 -30"
+                        />
+                    </g>
+                </g>
+            </g>
+        </svg>
+    )
+};
+
+export default GarzuzeMark
