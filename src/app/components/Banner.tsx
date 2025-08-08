aaf40d43570eb108cd2a743ad7df7441d9d52108
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-08-08 17:46:15 -0300

    feat: create Banner component

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Banner = () => {
+    return (
+        <div className="h-72 border-x border-stone-200 dark:border-stone-900">
+            <Image
+                src="/images/fog_croped.jpg"
+                alt="Banner image"
+                width={1080}
+                height={288}
+                className="h-full w-full object-cover object-center"
+            />
+        </div>
+    )
+};
+
+export default Banner
