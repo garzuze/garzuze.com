2d65d05493b3f8472f2d79bdcea1cd6a09ab5441
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-08-11 17:59:56 -0300

    feat: add SF font to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,23 @@
 import type { Metadata } from "next";
 import "./global.css";
 import { ThemeProvider } from './components/ThemeProvider';
+import localFont from 'next/font/local';
 
+const sf = localFont({
+  src: [
+    {
+      path: '../../public/fonts/sanfranciscodisplay-regular-webfont.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/sanfranciscodisplay-bold-webfont.woff',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+  variable: '--font-sf',
+})
 
 export const metadata: Metadata = {
   title: "garzuze - portfolio",
@@ -14,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={sf.className}>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
