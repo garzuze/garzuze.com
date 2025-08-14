96dddbe4ff3bd523e7441e9370a13674d9d6ccbf
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-08-14 17:35:44 -0300

    style: adjust responsiveness on Socials

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Socials = () => {
     return (
-        <div className="border-x border-stone-200 dark:border-stone-900 grid grid-cols-2 gap-x-[48px] h-[96px]">
-            <div className="w-[calc(100%+1px)] border-r border-stone-200 dark:border-stone-900 items-center grid px-[24px] group">
+        <div className="border-x border-stone-200 dark:border-stone-900 grid grid-cols-2 gap-x-[48px] sm:h-[96px]">
+            <div className="sm:w-[calc(100%+1px)] border-b sm:border-b-0 col-span-2 sm:col-span-1 py-4 sm:py-0 border-r border-stone-200 dark:border-stone-900 items-center grid px-[24px] group">
                 <a href="https://linkedin.com/in/garzuze" target="_blank" rel="noopener noreferrer">
                     <div className="flex space-x-[25px]">
                         <Image alt="LinkedIn" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" className="shrink-0" style={{ color: 'transparent' }} src="/images/linkedin.webp" />
@@ -16,8 +16,8 @@ const Socials = () => {
                     </div>
                 </a>
             </div>
-            <div className="border-l border-stone-200 dark:border-stone-900 items-center grid px-[24px] group">
-                <a href="https://linkedin.com/in/garzuze" target="_blank" rel="noopener noreferrer">
+            <div className="border-l py-4 sm:py-0 border-stone-200 col-span-2 sm:col-span-1 dark:border-stone-900 items-center grid px-[24px] group">
+                <a href="https://github.com/garzuze" target="_blank" rel="noopener noreferrer">
                     <div className="flex space-x-[25px]">
                         <Image alt="LinkedIn" loading="lazy" width="48" height="48" decoding="async" data-nimg="1" className="shrink-0" style={{ color: 'transparent' }} src="/images/github.webp" />
                         <div className="block">
