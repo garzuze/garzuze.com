2b8627a87cf0dd75a2d9cd196a2e45aa4de1e754
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-08-17 19:50:38 -0300

    feat: add images to projects section

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import MySeparator from "./my-separator"
 
 const Projects = () => {
@@ -13,23 +14,55 @@ const Projects = () => {
             </div>
             <MySeparator />
             <div className="w-[calc(100%+1px)] border-x border-stone-200 dark:border-stone-900 grid grid-cols-2 gap-x-[48px] sm:h-64">
-                <div className="sm:w-[calc(100%+1px)] border-b sm:border-b-0 col-span-2 sm:col-span-1 py-4 sm:py-0 border-r border-stone-200 dark:border-stone-900 items-center grid px-[24px] group">
+                <div className="sm:w-[calc(100%+1px)] border-b sm:border-b-0 col-span-2 sm:col-span-1 py-4 sm:py-0 border-r border-stone-200 dark:border-stone-900 grid px-[24px] group group-hover:bg-stone-950 cursor-pointer">
+                    <div className="mt-2">
+                        <Image
+                            src="/images/mlrh-light.png"
+                            width={1920}
+                            height={1080}
+                            alt="MLRH logo"
+                            className="border border-stone-200 dark:border-stone-900 rounded-sm dark:hidden block group"
+                        />
+                        <Image
+                            src="/images/mlrh.png"
+                            width={1920}
+                            height={1080}
+                            alt="MLRH logo"
+                            className="border border-stone-200 dark:border-stone-900 rounded-sm hidden dark:block"
+                        />
+                    </div>
                     <div>
                         <h2 className="scroll-m-20 text-xl font-semibold tracking-tight group-hover:underline cursor-pointer">
                             MLRH
                         </h2>
                         <p className="text-sm leading-none text-gray-500 dark:text-gray-300">
-                            All parts of HR in an unified interface
+                            Platform that centralizes all stages of HR process in a unified interface.
                         </p>
                     </div>
                 </div>
-                <div className="border-l py-4 sm:py-0 border-stone-200 col-span-2 sm:col-span-1 dark:border-stone-900 items-center grid px-[24px] group">
+                <div className="border-l py-4 sm:py-0 border-stone-200 col-span-2 sm:col-span-1 dark:border-stone-900 grid px-[24px] group cursor-pointer">
+                    <div className="mt-2">
+                        <Image
+                            src="/images/sepais-light.png"
+                            width={1920}
+                            height={1080}
+                            alt="Sepais' logo"
+                            className="border border-stone-200 dark:border-stone-900 rounded-sm dark:hidden block"
+                        />
+                        <Image
+                            src="/images/sepais.png"
+                            width={1920}
+                            height={1080}
+                            alt="Sepais' logo"
+                            className="border border-stone-200 dark:border-stone-900 rounded-sm hidden dark:block"
+                        />
+                    </div>
                     <div>
                         <h2 className="scroll-m-20 text-xl font-semibold tracking-tight group-hover:underline cursor-pointer">
                             Sepais
                         </h2>
                         <p className="text-sm leading-none text-gray-500 dark:text-gray-300">
-                            A system designed to streamline school communication
+                            A system designed to streamline school communication at IFPR.
                         </p>
                     </div>
                 </div>
