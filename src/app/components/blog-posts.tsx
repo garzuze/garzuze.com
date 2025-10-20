615871d6d57ae436e0d6421320d84499088e81a4
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-10-19 22:39:15 -0300

    chore: remove placholder blog posts

--- src/app/components/blog-posts.tsx
+++ src/app/components/blog-posts.tsx
@@ -20,7 +20,7 @@ const BlogPosts = () => {
 			<MySeparator />
 			<div className="gap-x-[48px] grid grid-cols-2 border-stone-200 dark:border-stone-900 w-[calc(100%+1px)]">
 				{posts.length === 0 ? (
-					<p>Vish nenhum post mano</p>
+					<p>Coming soon...</p>
 				) : (
 					posts.slice(0, 4).map((post, index) => (
 						<React.Fragment key={post.slug}>
