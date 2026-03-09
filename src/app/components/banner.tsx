645f0a82b50873f7bb7e0158d05054cd187e6a03
garzuze <lucasgarzuze@gmail.com>
2026-03-08 22:43:08 -0300

    feat: add special text to Banner component

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,8 +1,14 @@
 import AnimatedGradient from '@/components/animated-gradient';
+import { SpecialText } from '@/components/special-text';
 
 const Banner = () => {
   return (
     <div className="h-40 sm:h-52 border-x border-stone-200 dark:border-stone-900 relative    flex items-center justify-center">
+      <div className="z-10 flex flex-col items-center text-white gap-1">
+        <SpecialText speed={100} className="text-lg" inView>
+          غرزوز
+        </SpecialText>
+      </div>
       <AnimatedGradient style={{ zIndex: 0 }} config={{ preset: 'Vortex' }} />
     </div>
   );
